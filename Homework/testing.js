84d54f0b01927de1c5dd833f32cb2fa4ddc4a714
TrungNguyenDinh008 <113298530+TrungNguyenDinh008@users.noreply.github.com>
2022-12-13 23:24:46 +0700

    Add files via upload

---
+++ Homework/testing.js
@@ -0,0 +1,9 @@
+let x = prompt("Choose your favorite football country, please");
+let y = prompt("Choose an another country, please");
+let z = " in World Cup 2022 !"
+alert( "The "+ x +" Team" + " will win The " + y +" Team " + z);
+
+let a = prompt("Choose your favorite football team, now");
+let b = prompt("Choose an another team, again");
+let c = " in Premier League !"
+alert( "Ohhhhhh Shot: " + "The "+ a +" Team" + " will lose The " + b +" Team " + c);

82c7ecafaffb78371daac1b8ab07d860f3e0146a
TrungNguyenDinh008 <113298530+TrungNguyenDinh008@users.noreply.github.com>
2022-12-31 15:33:06 +0700

    Added Homework_30-Dec-2022

---
+++ Homework/Homework_30-Dec-2022/Homework1/calculator.js
@@ -0,0 +1,25 @@
+b = prompt("Type the first numbers");
+c = prompt("Type the second numbers");
+
+var b = Number(b)
+var c = Number(c)
+
+
+
+
+    var plus = (b + c);
+    alert(plus +" (plus calculation)");
+
+    var minus = (b - c);
+    alert(minus +" (minus calculation)");
+
+    var multiply = (b * c);
+    alert(multiply +" (multiply calculation)");
+
+    var divide = (b / c +" (divide calculation)");
+    alert(divide);
+
+    var modulo = (b % c +" (modulo calculation)");
+    alert(modulo);
+
+
---
+++ Homework/Homework_30-Dec-2022/Homework2/main.js
@@ -0,0 +1,15 @@
+var a = prompt("What is your name?");
+var b = a.slice(0,1);
+var b = b.toUpperCase();
+var c = a.slice(1,a.lenght);
+var c = c.toLowerCase();
+
+alert("Hello, " + b + c);
+
+
+
+var d = prompt("Post your Tweet here");
+var e = a.length;
+
+
+alert("You have already written " + e + " words. You can write another " + (280-e) + " words.")

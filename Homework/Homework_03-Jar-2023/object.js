a43eaeeda77a4c5ce8cf5887bfa3d4415c402d26
TrungNguyenDinh008 <113298530+TrungNguyenDinh008@users.noreply.github.com>
2023-01-05 13:53:07 +0700

    Added Homework_3-Jar-2023

---
+++ Homework/Homework_03-Jar-2023/object.js
@@ -0,0 +1,73 @@
+const appleWatchSE ={
+    name: " Apple Watch SE",
+    type: " Watch",
+    model: " Sport Watch",
+    operateSystem: " iOS 11",
+    color: " Black",
+    display:" Retina Display",
+    heartbeat:" Yes",
+    caloburn:" Yes",
+    sos:" Yes",
+    waterResistant:" 50 meter, 8atm",
+    fallDetection:" Yes",
+    crashDetection:" Yes",
+    cellular:" Yes",
+    des: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis orci in luctus commodo. Ut euismod quis massa quis sodales. Nulla lacinia, enim sed porta blandit, ante lacus lobortis nibh, dignissim dapibus lorem.",
+    price:" $249",
+    price10:" $24.9",
+}
+const appleWatchHermès ={
+    name: " Apple Watch Hermès",
+    type: " Watch",
+    model: " Deluxe Watch",
+    operateSystem: " iOS 11",
+    color: " Grey",
+    display:" Retina Display",
+    heartbeat:" Yes",
+    caloburn:" Yes",
+    sos:" Yes",
+    waterResistant:" 68 meter, 10atm",
+    fallDetection:" Yes",
+    crashDetection:" Yes",
+    cellular:" Yes",
+    des: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis orci in luctus commodo. Ut euismod quis massa quis sodales. Nulla lacinia, enim sed porta blandit, ante lacus lobortis nibh, dignissim dapibus lorem.",
+    price:" $559",
+    price10:" $55.9",
+    }
+
+    document.getElementById("name").innerHTML = appleWatchSE.name
+    document.getElementById("des").innerHTML = appleWatchSE.des
+    document.getElementById("price").innerHTML = appleWatchSE.price
+    document.getElementById("color").innerHTML = appleWatchSE.color
+    document.getElementById("display").innerHTML = appleWatchSE.display
+    document.getElementById("heartbeat").innerHTML = appleWatchSE.heartbeat
+    document.getElementById("caloburn").innerHTML = appleWatchSE.caloburn
+    document.getElementById("sos").innerHTML = appleWatchSE.sos
+    document.getElementById("waterResistant").innerHTML = appleWatchSE.waterResistant
+    document.getElementById("fallDetection").innerHTML = appleWatchSE.fallDetection
+    document.getElementById("crashDetection").innerHTML = appleWatchSE.crashDetection
+    document.getElementById("cellular").innerHTML = appleWatchSE.cellular
+    document.getElementById("operateSystem").innerHTML = appleWatchSE.operateSystem
+
+
+    document.getElementById("name2").innerHTML = appleWatchHermès.name
+    document.getElementById("des2").innerHTML = appleWatchHermès.des
+    document.getElementById("price2").innerHTML = appleWatchHermès.price
+    document.getElementById("color2").innerHTML = appleWatchHermès.color
+    document.getElementById("display2").innerHTML = appleWatchHermès.display
+    document.getElementById("heartbeat2").innerHTML = appleWatchHermès.heartbeat
+    document.getElementById("caloburn2").innerHTML = appleWatchHermès.caloburn
+    document.getElementById("sos2").innerHTML = appleWatchHermès.sos
+    document.getElementById("waterResistant2").innerHTML = appleWatchHermès.waterResistant
+    document.getElementById("fallDetection2").innerHTML = appleWatchHermès.fallDetection
+    document.getElementById("crashDetection2").innerHTML = appleWatchHermès.crashDetection
+    document.getElementById("cellular2").innerHTML = appleWatchHermès.cellular
+    document.getElementById("operateSystem2").innerHTML = appleWatchHermès.operateSystem
+
+    
+
+
+
+
+
+
